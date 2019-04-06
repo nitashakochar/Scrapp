94a2053958a0cdc09759e8d7480f00d464c19d0a
Nitasha Kochar <nitashakochar@gmail.com>
2019-04-06 14:39:33 -0400

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React!</h2>
+        <button color="blue" shadowSize={2}>
+          Click Me
+        </button>
+      </div>
+    );
+  }
+}
