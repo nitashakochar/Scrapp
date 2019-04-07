2e85cce62d621ed65cdb1ca7aa289de9469a9e3d
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-04-06 20:44:47 -0400

    initial read from file

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,10 @@ import Entry from "./entry";
 import Journal from "./journal";
 import Nav from "./Nav";
 
+// for writing to and reading from file
+var fs = require('fs');
+var filename = process.cwd() + "/entries.json";
+
 
 const entryData = { people: ["Anna", "Shep"],
                     places: ["Dartmouth", "Thayer"], 
@@ -13,19 +17,65 @@ const entryData = { people: ["Anna", "Shep"],
                     image: "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Thayer_School_of_Engineering_shield.svg/1920px-Thayer_School_of_Engineering_shield.svg.png",
                     };
 
+const entries = [
+  {
+    people: ['Nitasha', 'Regina'],
+    places: ['Spanos', 'Hitchcock'],
+    journals: ['sad', 'major'],
+    tags: ['stress', 'fun'],
+    title: 'first entry',
+    content: 'hello world',
+  },
+  {
+    people: ['Nitasha', 'Regina'],
+    places: ['Thayer', 'Hitchcock'],
+    journals: ['sad', 'major'],
+    tags: ['stress', 'fun'],
+    title: 'second entry',
+    content: 'hello world',
+  },
+];
+
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      all: [],
+      loaded: false,
+    }
+  }
+
+  componentWillMount() {
+    // fs.writeFile(filename, JSON.stringify(entries), function (err) {
+    //   if (err) throw err;
+    //   console.log(filename);
+    // });
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) throw err;
+      let allE = JSON.parse(data);
+      this.setState({all: allE, loaded: true});
+    });
+
+  }
+
   render() {
-    return (
-      <div>
-        <Nav></Nav>
-        <Journal />
-        <h2>Welcome to React!</h2>
-        <button color="blue" shadowSize={2}>
-          Click Me
-        </button>
-        <Entry display="full" entry={entryData} />
-        <Entry display="cover" entry={entryData}/>
-      </div>
-    );
+    if (this.state.loaded) {
+      return (
+        <div>
+          <Nav></Nav>
+          <Journal />
+          <h2>Welcome to React!</h2>
+          <button color="blue" shadowSize={2}>
+            Click Me
+          </button>
+          <Entry display="full" entry={entryData} />
+          <Entry display="cover" entry={this.state.all[1]}/>
+        </div>
+      );
+    }
+    else {
+      return(<div>Loading...</div>);
+    }
+
   }
 }
